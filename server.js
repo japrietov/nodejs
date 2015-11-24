a8aefeb360013a92f2f8c1f4e8863d9a12bd3652
Koding <crorodiguezro@unal.edu.co>
2015-11-24 19:55:11 +0000

    base

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0");
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('url: ' + req.query.url);
+});
 
 http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
     console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
