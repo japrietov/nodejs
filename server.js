c3ec70e2ad393759303fac11ba34bc16afb79d87
Koding <crorodiguezro@unal.edu.co>
2015-11-24 17:37:57 +0000

    base

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-// Load module dependencies
-var http = require('http')
+var express = require('express')
+var app = express()
 
-http.createServer(function(request, response) {
-        response.end("Hola");
-}).listen(80);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
-console.log('node server running on port 3000');
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
