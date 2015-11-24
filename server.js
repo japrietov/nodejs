a979bb386d453e4c9a887b18787b54128a2f9312
Koding <crorodiguezro@unal.edu.co>
2015-11-24 18:02:36 +0000

    base

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 var express = require('express')
 var app = express()
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+app.set('port', port);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP);
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-var server = app.listen(3000, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
+var server = app.listen(port, function () {
 
 })
